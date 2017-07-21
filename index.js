2d580482c6df465755405f4af77e9f0d5d3796d2
Nathan Ondracek <nondracek@Nathans-MacBook-Pro-5.local>
2017-07-21 13:18:49 -0500

    H4

---
+++ index.js
@@ -0,0 +1,23 @@
+
+var cool = require('cool-ascii-faces');
+import express from 'express';
+// import morgan from 'morgan';
+import router from './router';
+
+app.set('port', (process.env.PORT || 5000));
+
+// Initialize http server
+const app = express();
+
+// Logger that outputs all requests into the console
+// app.use(morgan('combined'));
+// Use router for all API endpoints
+// app.use('/', router);
+
+app.get('/', function(request, response) {
+  response.send(cool())
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
